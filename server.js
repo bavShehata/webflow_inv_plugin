a0bc265722f89a6da764d181801304419ab484e9
BavShehata <bavly@bavlifyweb.com>
2023-07-14 09:37:18 +0300

    Fixing memory leaks

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 //Environment variables
-var cron = require("node-cron");
-const jsdom = require("jsdom");
+const cron = require("node-cron");
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT ?? 8001;
-const { XMLParser, XMLValidator } = require("fast-xml-parser");
+const { XMLParser } = require("fast-xml-parser");
 const fs = require("fs");
 const cloudinary = require("cloudinary");
-const stream = require("stream");
-const { promisify } = require("util");
-
-const finished = promisify(stream.finished);
-
-xml2js = require("xml2js");
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 // middleware & static files
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(express.json());
 
 cloudinary.config({
   cloud_name: "du7e4ltoq",
@@ -34,7 +25,7 @@ cloudinary.config({
   api_secret: "ixMlmmwQfm1BKclbbPLXn5AuNtU",
 });
 //Constants and variables
-var siteId = "64a57674f0a3977d20e3865b";
+const siteId = "64a57674f0a3977d20e3865b";
 const config = {
   headers: {
     Authorization: `Bearer 3588c873fdc9a472583c06d2e04973049d787e20e7fa54e6485fad715ce4753f`,
@@ -42,20 +33,18 @@ const config = {
     "content-type": "application/json",
   },
 };
-var collectionId = "64aaff5935018faa5a99900f";
+const collectionId = "64aaff5935018faa5a99900f";
 const domains = {
   domains: ["carmoji-ca.webflow.io"],
 };
 
 const REQUEST_DELAY = 1000;
-var url,
-  result,
-  numOfItems,
-  remaining,
-  xmlData,
-  vehicles,
-  uploadedItems,
-  deletedItems;
+let numOfItems = 0,
+  remaining = 0,
+  dealership_info = {},
+  vehicles = [],
+  uploadedItems = 0,
+  deletedItems = 0;
 
 // Call the script
 app.get("/script", async (req, res, next) => {
@@ -69,8 +58,7 @@ app.all("", async (req, res) => {
   cleanup();
 });
 const runScript = () => {
-  // Resetting variables
-  uploadedItems = deletedItems = 0;
+  resetVariables();
   // Fetching old items to delete
   fetchAllItems();
 };
@@ -78,20 +66,20 @@ const runScript = () => {
 const fetchAllItems = async () => {
   url = `https://api.webflow.com/collections/${collectionId}/items`;
   try {
-    var items = await axios.get(url, config);
+    const items = await axios.get(url, config);
     console.log("ITEMS FETCHED SUCCESSFULLY");
-    items = items.data.items;
-    numOfItems = items.length;
+    const data = items.data.items;
+    numOfItems = data.length;
     console.log("Number of current items is: ", numOfItems);
     console.log("First, delete all items");
-    deleteItems(items);
+    deleteItems(data);
   } catch (e) {
     console.log("Couldn't fetch all items ", e);
   }
 };
 // Recursively deleting all items
 const deleteItems = async (items) => {
-  if (numOfItems != 0) {
+  if (numOfItems > 0) {
     const item = items[deletedItems];
     if (!item) return;
     const itemId = item._id;
@@ -101,7 +89,7 @@ const deleteItems = async (items) => {
       " out of ",
       numOfItems
     );
-    url = `https://api.webflow.com/collections/${collectionId}/items/${itemId}`;
+    const url = `https://api.webflow.com/collections/${collectionId}/items/${itemId}`;
     try {
       await axios.delete(url, config);
       deletedItems++;
@@ -117,7 +105,7 @@ const deleteItems = async (items) => {
       deletedItems
     );
     // Publishing the website before adding new items
-    url = `https://api.webflow.com/sites/${siteId}/publish`;
+    const url = `https://api.webflow.com/sites/${siteId}/publish`;
     axios
       .post(url, domains, config)
       .then(async () => {
@@ -129,32 +117,49 @@ const deleteItems = async (items) => {
 };
 // Update Webflow CMS via the script
 const updateCMS = async () => {
-  url = "http://mediafeed.boostmotorgroup.com/2438/export.xml";
+  const url = "http://mediafeed.boostmotorgroup.com/2438/export.xml";
   const result = await axios.get(url);
   const data = result.data;
   const parser = new XMLParser({ ignoreAttributes: false });
-  var json = parser.parse(data);
-  xmlData = json["Datafeed"]["Dealership"];
-  vehicles = xmlData["Inventory"]["Vehicle"];
+  const json = parser.parse(data);
+  const dealership = json["Datafeed"]["Dealership"];
+  // Dealership data
+  dealership_info = {
+    Dealership_Boost_ID: dealership["Dealership_Boost_ID"],
+    Dealership_Other_ID: dealership["Dealership_Other_ID"],
+    Dealership_Name: dealership["Dealership_Name"],
+    Dealership_Website: dealership["Dealership_Website"],
+    Dealership_Phone: dealership["Dealership_Phone"],
+    Dealership_Email: dealership["Dealership_Email"],
+    Dealership_Address: dealership["Dealership_Address"],
+    Dealership_City: dealership["Dealership_City"],
+    Dealership_Postal: dealership["Dealership_Postal"],
+    Dealership_Province: dealership["Dealership_Province"],
+    Dealership_Latitude: dealership["Dealership_Latitude"],
+    Dealership_Longitude: dealership["Dealership_Longitude"],
+  };
+  vehicles = dealership["Inventory"]["Vehicle"];
   numOfItems = vehicles.length;
-  remaining = 0;
   console.log("Total number of items: ", numOfItems);
+
+  // Delete to avoid memory leak
+  delete json, dealership, parser;
   // XML data retrieved
   addNextItem();
 };
 // Adding an item to webflow
 const addNextItem = async () => {
   try {
-    item = await getNextItem();
+    const item = await getNextItem();
 
-    url = `https://api.webflow.com/collections/${collectionId}/items`;
-    var data = JSON.stringify({ fields: item });
+    const url = `https://api.webflow.com/collections/${collectionId}/items`;
+    const data = JSON.stringify({ fields: item });
     try {
       // Adding the item to webflow
       console.log(
         "Uploading item " + ++uploadedItems + " of " + numOfItems + " items."
       );
-      result = await axios.post(url, data, config);
+      const result = await axios.post(url, data, config);
       console.log("Item uploaded successfully");
       if (uploadedItems < numOfItems) setTimeout(addNextItem, REQUEST_DELAY);
       else cleanup();
@@ -174,39 +179,20 @@ const addNextItem = async () => {
 };
 // Returns the next item after changing it to JSON
 const getNextItem = async () => {
-  var vehicle = vehicles[remaining++];
-  dealership_data = xmlData;
-  // Dealership data
-  dealership = {
-    Dealership_Boost_ID: dealership_data["Dealership_Boost_ID"],
-    Dealership_Other_ID: dealership_data["Dealership_Other_ID"],
-    Dealership_Name: dealership_data["Dealership_Name"],
-    Dealership_Website: dealership_data["Dealership_Website"],
-    Dealership_Phone: dealership_data["Dealership_Phone"],
-    Dealership_Email: dealership_data["Dealership_Email"],
-    Dealership_Address: dealership_data["Dealership_Address"],
-    Dealership_City: dealership_data["Dealership_City"],
-    Dealership_Postal: dealership_data["Dealership_Postal"],
-    Dealership_Province: dealership_data["Dealership_Province"],
-    Dealership_Latitude: dealership_data["Dealership_Latitude"],
-    Dealership_Longitude: dealership_data["Dealership_Longitude"],
-  };
+  const vehicle_info = vehicles[remaining++];
 
-  vehicle_info = {};
-  features = [];
-
-  vehicle = { ...dealership, ...vehicle };
+  let vehicle = { ...dealership_info, ...vehicle_info };
   // use the first image as the main one
-  var imageGallery = [];
-  main_img_url =
+  let imageGallery = [];
+  let main_img_url =
     "https://media.getedealer.com/w_400,h_300,q_90,c_l,v1/inventory/6RHU7CPFWRG7TFNE6JCXOPN42Y.webp";
   if (vehicle["Images"]["@_count"] == "1")
     main_img_url = vehicle["Images"]["Photo"]["#text"];
   else if (vehicle["Images"]["@_count"] != "0") {
     // use the next maximum of 25 images in the multi-image field
     main_img_url = vehicle["Images"]["Photo"][0]["#text"];
-    images = vehicle["Images"]["Photo"].slice(1);
-    i = 0;
+    const images = vehicle["Images"]["Photo"].slice(1);
+    let i = 0;
     images.every((image) => {
       if (i == 25) return false;
       imageGallery.push({ url: image["#text"] });
@@ -253,7 +239,7 @@ const getNextItem = async () => {
     "name",
     "dealership-boost-id",
   ];
-  vehicle_temp = {};
+  let vehicle_temp = {};
   // set keys to lowercase and replace underscore with a dash (to match webflow naming)
   for (const key in vehicle)
     vehicle_temp[key.toLowerCase().replace("_", "-")] = vehicle[key];
@@ -278,7 +264,7 @@ const getNextItem = async () => {
     console.log("main image reuploaded");
     // reupload gallery images
     console.log("Reuploading IMAGE GALLERY");
-    for (var i = 0; i < vehicle["images"].length; i += 1)
+    for (let i = 0; i < vehicle["images"].length; i += 1)
       vehicle["images"][i]["url"] = await reuploadImage(
         vehicle["images"][i]["url"]
       );
@@ -290,14 +276,13 @@ const getNextItem = async () => {
 };
 // Download the image, upload it to cloudinary, save the URL, then delete it from the server.
 const reuploadImage = async (boost_url) => {
-  boost_url;
-  filepath = boost_url.split("/").slice(-1)[0].replace(/%20/g, "_");
-  filename = filepath.split(".")[0];
+  const filepath = boost_url.split("/").slice(-1)[0].replace(/%20/g, "_");
+  const filename = filepath.split(".")[0];
   console.log("downloading image ", filename);
   try {
     await downloadImage(boost_url, filepath);
     // console.log("Image downloaded");
-    result = await cloudinary.v2.uploader.upload(filepath, {
+    const result = await cloudinary.v2.uploader.upload(filepath, {
       public_id: filename,
     });
     // console.log("Image uploaded: ", result.url);
@@ -323,18 +308,25 @@ async function downloadImage(url, filepath) {
 }
 // delete cloudinary images and publish the website
 const cleanup = async () => {
-  url = `https://api.webflow.com/sites/${siteId}/publish`;
+  resetVariables();
+  const url = `https://api.webflow.com/sites/${siteId}/publish`;
   axios
     .post(url, domains, config)
     .then(async () => {
       console.log("Website Published Successfully!");
     })
     .catch((e) => console.log("Couldn't publish website, ", e));
-  result = await cloudinary.v2.api.delete_all_resources();
+  const result = await cloudinary.v2.api.delete_all_resources();
   console.log("All cloud images deleted. Clean up completed");
 };
+// Resetting variables
+const resetVariables = () => {
+  numOfItems = remaining = uploadedItems = deletedItems = 0;
+  dealership_info = {};
+  vehicles = [];
+};
 // Will execute every day at midnight GMT-5
-var automatedScript = cron.schedule(
+cron.schedule(
   "0 0 0 * * *",
   () => {
     console.log(
